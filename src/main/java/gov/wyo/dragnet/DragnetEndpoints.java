61d7f3b5dfa6f1407e40aa6ecc2b4da1805d2df8
sabotuer99 <sabotuer99@gmail.com>
2016-07-11 11:36:17 -0600

    initial commit

---
+++ src/main/java/gov/wyo/dragnet/DragnetEndpoints.java
@@ -0,0 +1,39 @@
+package gov.wyo.dragnet;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiMethod.HttpMethod;
+import com.google.api.server.spi.config.Named;
+
+/**
+ * Defines endpoint functions APIs.
+ */
+@Api(name = "dragnetendpoints", version = "v1",
+scopes = {Constants.EMAIL_SCOPE },
+        clientIds = {Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID },
+        description = "API for dragnet endpoints.")
+
+public class DragnetEndpoints {
+
+   // Declare this method as a method available externally through Endpoints
+    @ApiMethod(name = "sayHello", path = "sayHello",
+            httpMethod = HttpMethod.GET)
+
+    public HelloClass sayHello() {
+        return new HelloClass();
+    }
+
+    // Declare this method as a method available externally through Endpoints
+    @ApiMethod(name = "sayHelloByName", path = "sayHelloByName",
+            httpMethod = HttpMethod.GET)
+
+    public HelloClass sayHelloByName (@Named("name") String name) {
+        return new HelloClass(name);
+    }
+    
+    @ApiMethod(name = "greetByPeriod", path = "greetByPeriod",
+            httpMethod = HttpMethod.GET)
+    public HelloClass greetByPeriod (@Named("name") String name, @Named("period") String period) {
+        return new HelloClass(name, period);
+    }
+}
